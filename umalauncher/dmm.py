2ca887139927590f69a899e7f4abfb63156eebff
KevinVG207 <19467234+KevinVG207@users.noreply.github.com>
2023-07-15 21:48:22 +0200

    Find new DMM window handle name. (#142)
    New DMM window handle name.

--- umalauncher/dmm.py
+++ umalauncher/dmm.py
@@ -7,7 +7,7 @@ def start():
     os.system("Start dmmgameplayer://play/GCL/umamusume/cl/win")
 
 def get_dmm_handle():
-    for window_title in ['DMM GAME PLAYER', 'マイゲーム']:
+    for window_title in ['ストアトップ | 一般', 'DMM GAME PLAYER', 'マイゲーム']:
         dmm_handle = util.get_window_handle(window_title, type=util.LAZY)
         if dmm_handle:
             return dmm_handle
